b740724e66f91636b3b5469458c5a2466e012f27
Muslim Mamadaev <mamad.muslim@mail.ru>
2025-01-26 17:38:45 +0300

    update legal notice and privacy policy pages

--- festival-platform/src/pages/LegalNoticePage/LegalNoticePage.jsx
+++ festival-platform/src/pages/LegalNoticePage/LegalNoticePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Typography } from "antd";
 import { useTranslation } from "react-i18next";
 
-const { Title } = Typography;
+const { Title, Paragraph } = Typography;
 
 const PrivacyPolicyPage = () => {
   const { t } = useTranslation();
@@ -10,7 +10,13 @@ const PrivacyPolicyPage = () => {
   return (
     <div className="privacy-policy-container">
       <Typography>
-        <Title level={2}>{t("legalNoticeTitle")}</Title>
+        <Title level={2}>{t("impressum")}</Title>
+        <Title level={4}>{t("Oktoberfest Tours by Julie")}</Title>
+
+        <Paragraph>{t("Franz-Wolter-Straße 42")}</Paragraph>
+        <Paragraph>{t("81925 München")}</Paragraph>
+        <Paragraph>{t("Geschäftsführerin: Julietta Maruschkin")}</Paragraph>
+        <Paragraph>{t("+4917642024296")}</Paragraph>
       </Typography>
     </div>
   );
--- festival-platform/src/pages/PrivacyPolicyPage/PrivacyPolicyPage.jsx
+++ festival-platform/src/pages/PrivacyPolicyPage/PrivacyPolicyPage.jsx
@@ -11,17 +11,13 @@ const PrivacyPolicyPage = () => {
   return (
     <div className="privacy-policy-container">
       <Typography>
-        <Title level={2}>{t("privacyPolicyTitle")}</Title>
+        <Title level={2}>{t("privacyPolicy")}</Title>
+        <Title level={4}>{t("Oktoberfest Tours by Julie")}</Title>
 
-        <Paragraph>{t("privacyPolicyIntroduction")}</Paragraph>
-
-        <Title level={4}>{t("section1Title")}</Title>
-        <Paragraph>{t("section1Content")}</Paragraph>
-
-        <Title level={4}>{t("section2Title")}</Title>
-        <Paragraph>{t("section2Content")}</Paragraph>
-
-        <Paragraph>{t("privacyPolicyConclusion")}</Paragraph>
+        <Paragraph>{t("Franz-Wolter-Straße 42")}</Paragraph>
+        <Paragraph>{t("81925 München")}</Paragraph>
+        <Paragraph>{t("Geschäftsführerin: Julietta Maruschkin")}</Paragraph>
+        <Paragraph>{t("+4917642024296")}</Paragraph>
       </Typography>
     </div>
   );
